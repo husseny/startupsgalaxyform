65c9ab412f270649e11ddc3566b412631dea4b25
Husseny <abdullrahman.elhusseni@gmail.com>
2017-11-09 00:41:10 +0200

    json format

--- form/views.py
+++ form/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 import json
 
